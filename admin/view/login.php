79b2fdd76abb26f41f1f502463839436b012c195
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-04-12 19:33:10 -0600

    Added login admin page

---
+++ admin/view/login.php
@@ -0,0 +1,11 @@
+<?php
+//if($login_message)
+?>
+<form action="." method="GET" class="register_form">
+    <input type="hidden" name="action" value="login">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" maxlength="50" required>
+    <label for="password">Password:</label>
+    <input type="text" id="password" name="password" maxlength="50" required>
+    <input type="submit" value="Login" class="button blue">
+</form>
