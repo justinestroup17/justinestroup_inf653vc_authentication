6afdd659486c60ff5e1769f8f333ce8d1626ab3a
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-04-13 21:31:49 -0600

    Heroku Update 2

--- model/database.php
+++ model/database.php
@@ -14,10 +14,10 @@
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
-        $db = new PDO($dsn, $username);
+        //$db = new PDO($dsn, $username);
 
         // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error = "Database Error: ";
         $error .= $e->getMessage();
